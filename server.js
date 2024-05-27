e44dbe9567e6f70a6528290717b0189a0ce887e4
Udodirim <Udodirimwisdom@gmail.com>
2024-05-27 17:17:09 +0100

    updated

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = require("./app");
 
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  console.log("shutting down the server due to uncaught exception");
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE.replace(
@@ -17,9 +23,17 @@ mongoose
   .then((con) => {
     console.log("DB Connection Established!");
   });
-  console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  // console.log(err.name, err.message);
+  console.log("shutting down the server");
+  server.close(() => {
+    process.exit(1);
+  });
 });
